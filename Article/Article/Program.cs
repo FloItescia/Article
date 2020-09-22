e1d21753114f4f1e63a29a7ea4eeb526a84898f0
Florian Mandar <florian.mandar@hotmail.fr>
2020-09-22 12:30:15 +0200

    gt

---
+++ Article/Article/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Article;
+
+namespace Article
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            article article1;
+            article article2;
+
+            article1("PS4", 200, 2);
+        }
+    }
+}
---
+++ Article/Article/article.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Article
+{
+    struct article
+    {
+        public string nom;
+        public double prix;
+        public int quantite;
+
+        article(string nom, double prix, int quantite)
+        {
+            this.nom = nom;
+            this.prix = prix;
+            this.quantite = quantite;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("L'article {0} est au prix de {1}, la quantité restante est de {2}", this.nom, this.prix, this.quantite);
+        }
+
+        public void Ajouter()
+        {
+            this.quantite += 1;
+        }
+
+        public void Retirer()
+        {
+            if (this.quantite >= 1)
+            {
+                this.quantite -= 1;
+            }
+            else
+            {
+                Console.WriteLine("Plus aucune quantité n'est disponible.");
+            }
+        }
+    }
+}
