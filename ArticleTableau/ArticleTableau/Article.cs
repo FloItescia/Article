4e511211e40b201f31eb921c9c50ddde41ccb6d2
Florian Mandar <florian.mandar@hotmail.fr>
2020-09-22 14:58:25 +0200

    Finalisation

---
+++ ArticleTableau/ArticleTableau/Article.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticleTableau
+{
+    public enum ArticleEnum
+    {
+        Alimentaire,
+        Droguerie,
+        Habillement,
+        Loisir
+    }
+    public struct Article
+    {
+        private string nomArticle;
+        private double prixArticle;
+        private int quantiteArticle;
+        private ArticleEnum _typeArticleEnum;
+
+        public Article(string nomArticle, double prixArticle, int quantiteArticle, ArticleEnum typeArticleEnum)
+        {
+            this.nomArticle = nomArticle;
+            this.prixArticle = prixArticle;
+            this.quantiteArticle = quantiteArticle;
+            this._typeArticleEnum = typeArticleEnum;
+        }
+
+
+        public void Afficher()
+        {
+            Console.WriteLine("[Affichage Article]");
+            Console.WriteLine("Nom de l'article : " + nomArticle);
+            Console.WriteLine("Prix de l'article : " + prixArticle + " euros");
+            Console.WriteLine("Quantité de l'Article : " + quantiteArticle);
+
+            if (_typeArticleEnum == ArticleEnum.Alimentaire)
+            {
+                Console.WriteLine("Type de l'article : Alimentaire");
+            }
+
+            if (_typeArticleEnum == ArticleEnum.Droguerie)
+            {
+                Console.WriteLine("Type de l'article : Droguerie");
+            }
+
+            if (_typeArticleEnum == ArticleEnum.Habillement)
+            {
+                Console.WriteLine("Type de l'article : Habillement");
+            }
+
+            if (_typeArticleEnum == ArticleEnum.Loisir)
+            {
+                Console.WriteLine("Type de l'article : Loisir");
+            }
+
+        }
+
+        public void Ajouter()
+        {
+            quantiteArticle++;
+            Console.WriteLine("[Ajout Article]");
+        }
+
+        public void Retirer()
+        {
+            if (quantiteArticle != 0)
+            {
+                quantiteArticle--;
+                Console.WriteLine("[Suppression Article]");
+                Console.WriteLine("Vous avez supprimez un article, il reste " + quantiteArticle + " unité de l'article " + nomArticle);
+            }
+            else
+            {
+                Console.WriteLine("[Suppression Article]");
+                Console.WriteLine("Erreur : Il n'y a plus d'article disponnible");
+            }
+
+        }
+    }
+}
---
+++ ArticleTableau/ArticleTableau/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArticleTableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var article1 = new Article("NBA 2K21", 69.99, 2, ArticleEnum.Loisir);
+            var article2 = new Article("Pomme", 1, 50, ArticleEnum.Alimentaire);
+            var article3 = new Article("Veste", 60, 6, ArticleEnum.Habillement);
+
+            var tableauArticle = new Article[] { article1, article2, article3 };
+
+            for (int i = 0; i < tableauArticle.Length; i++)
+            {
+                tableauArticle[i].Afficher();
+            }
+
+            Console.WriteLine("[Modification quantitée Veste]");
+            tableauArticle[2].Retirer();
+            tableauArticle[2].Afficher();
+        }
+    }
+}
